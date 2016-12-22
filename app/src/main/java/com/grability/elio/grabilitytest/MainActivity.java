68219288542a128b735bb574e6b146bb3c596c72
elio <e1987ebrb@gmail.com>
2016-12-21 20:20:58 -0400

    Initial commit

---
+++ app/src/main/java/com/grability/elio/grabilitytest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.grability.elio.grabilitytest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtHello)
+    TextView title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        title.setText("Hello world!");
+    }
+}
