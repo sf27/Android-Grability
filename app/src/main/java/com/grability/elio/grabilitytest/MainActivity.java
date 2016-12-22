e5c4ee8ed01a331fa68576c807e416d4d5a0a4bd
elio <e1987ebrb@gmail.com>
2016-12-21 20:56:41 -0400

    Working API

--- app/src/main/java/com/grability/elio/grabilitytest/MainActivity.java
+++ app/src/main/java/com/grability/elio/grabilitytest/MainActivity.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.grability.elio.grabilitytest.api.ApiClient;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         title.setText("Hello world!");
+
+        Call<ApiClient.Objs> call = ApiClient.getObjects();
+        call.enqueue(new Callback<ApiClient.Objs>() {
+            @Override
+            public void onResponse(Call<ApiClient.Objs> call, Response<ApiClient.Objs> response) {
+                title.setText(response.body().feed.entry.get(0).title.label);
+            }
+
+            @Override
+            public void onFailure(Call<ApiClient.Objs> call, Throwable t) {
+            }
+        });
     }
 }
